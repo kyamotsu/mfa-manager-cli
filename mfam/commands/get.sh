d2db98fd7c381ff3cd79c9e32866f475258285ef
kyamotsu <kyamotu_kyamotu@yahoo.co.jp>
2023-07-06 21:59:48 +0900

    add get command

---
+++ mfam/commands/get.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euvx -o pipefail
+shopt -s inherit_errexit
+
+################################################################################
+# Description
+# ==========
+# Get one time password.
+#
+# Args
+# ==========
+# 1. APP_NAME
+#    e.g.) xxx
+#
+################################################################################
+: $1
+
+APP_NAME=$1
+
+pushd $(dirname $0) > /dev/null
+trap "popd > /dev/null" EXIT
+
+SECRET=$(cat ../data/${APP_NAME}.json | jq -r .secret)
+
+oathtool --base32 --totp "${SECRET}"
